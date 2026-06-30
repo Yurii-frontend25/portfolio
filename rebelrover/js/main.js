2b9996f7cf0a6ab9c5df04ff72c632cf650c4edf
Yurii-frontend25 <urusavskii@gmail.com>
2026-06-30 18:16:40 +0300

    edited files

--- rebelrover/js/main.js
+++ rebelrover/js/main.js
@@ -52,15 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 });
 //====================================================================================================
-// $(function () {
-// 	$('.block__title').click(function (event) {
-// 		if ($('.block').hasClass('one')) {
-// 			$('.block__title').not($(this)).removeClass('active');
-// 			$('.block__text').not($(this).next()).slideUp(300);
-// 		}
-// 		$(this).toggleClass('active').next().slideToggle(300);
-// 	});
-// });
 //====================================================================================================
 const exploreSlider = new Swiper('.explore__slider', {
 	observer: true,
